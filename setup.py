964130286842515effb17585bf796d02c46b9300
Hamdi Sahloul <hamdisahloul@hotmail.com>
2016-11-03 17:15:04 +0900

    Preparing for packaging

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 #
 #    You should have received a copy of the GNU Lesser General Public License
 #    along with EAV-Django.  If not, see <http://gnu.org/licenses/>.
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-eav',
